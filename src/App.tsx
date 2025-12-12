8bef09b6c1596882806458fc60687e9bfc762de3
22130217LamHongPhuc <22130217@st.hcmuaf.edu.vn>
2025-12-12 16:59:53 +0700

    Structure intial project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css'
+function App() {
+  return (
+    <h1 className="text-3xl text-red font-bold underline">
+                    Hello world!
+    </h1>
+  )
+}
+
+export default App;
