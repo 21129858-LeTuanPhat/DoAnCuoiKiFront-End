b08931b66b01782ca91d313374d808fc07bf3b6f
PhamThanhTai-22130242 <22130242@st.hcmuaf.edu.vn>
2025-12-15 10:49:50 +0700

    xu li loi

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FormLogin from '../components/auth/FormLogin'
+
+export default function SignInPage() {
+    const logo: string = 'https://danhgiacongnghe.net/wp-content/uploads/2024/10/ung-dung-zalo-thumb.jpg'
+    return (
+        <>
+            <div>
+                <FormLogin></FormLogin>
+            </div>
+        </>
+    )
+}
