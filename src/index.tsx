bcba56453763463aa44aed659b18f5e43d24b2fa
PhamThanhTai-22130242 <22130242@st.hcmuaf.edu.vn>
2025-12-20 13:00:32 +0700

    fix coflict

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,16 @@ import reportWebVitals from './reportWebVitals';
 import SocketWrap from './socket/SocketWrap';
 
 import { BoardProvider } from './components/chat/Context/BoardProvider';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     // <React.StrictMode>
     <BoardProvider>
         <SocketWrap>
-            <App />
+            <Provider store={store}>
+                <App /></Provider>
         </SocketWrap>
     </BoardProvider>,
     // </React.StrictMode>,
