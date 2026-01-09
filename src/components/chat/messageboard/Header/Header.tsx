58a5badfabfadc8c6666e24c0442b19f4b5d67a8
Le Tuan Phat <21129858@st.hcmuaf.edu.vn>
2026-01-09 14:56:06 +0700

    fix loi

--- src/components/chat/messageboard/Header/Header.tsx
+++ src/components/chat/messageboard/Header/Header.tsx
@@ -1,4 +1,3 @@
-
 import { PanelLeft } from 'lucide-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faVideo } from '@fortawesome/free-solid-svg-icons';
@@ -11,13 +10,23 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../../../../redux/store';
 import { CallStatus } from '../../../../model/CallProps';
 import { Dispatch } from '@reduxjs/toolkit';
-function Header({ username, setOpen, setTypeCalling }: { username: string, setOpen: React.Dispatch<React.SetStateAction<boolean>>, setTypeCalling: React.Dispatch<React.SetStateAction<number>> }) {
+import ListUserGroup from './ListUserGroup';
+function Header({
+    username,
+    setOpen,
+    setTypeCalling,
+}: {
+    username: string;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    setTypeCalling: React.Dispatch<React.SetStateAction<number>>;
+}) {
     // const paddingTop = 50;
     // const paddingLeft = 100;
     // const width = window.innerWidth - paddingLeft * 2;
     // const height = window.innerHeight - paddingTop * 2;
 
-    const { selectedUser } = useBoardContext();
+    const { selectedUser, type: typeMessage } = useBoardContext();
+    const [openPanel, setOpenPanel] = useState<boolean>(false);
     const hanldeVoice = () => {
         setOpen(true);
         setTypeCalling(TypeMess.VOICE_CALL);
@@ -25,9 +34,8 @@ function Header({ username, setOpen, setTypeCalling }: { username: string, setOp
     const hanldeVideo = () => {
         setOpen(true);
         setTypeCalling(TypeMess.VIDEO_CALL);
-
     };
-    const selector = useSelector((state: RootState) => state.call)
+    const selector = useSelector((state: RootState) => state.call);
     useEffect(() => {
         if (selector.callStatus === CallStatus.REJECT) {
             setOpen(false);
@@ -35,11 +43,17 @@ function Header({ username, setOpen, setTypeCalling }: { username: string, setOp
     }, [selector.callStatus]);
     return (
         <>
+            {openPanel && <ListUserGroup openPanel={openPanel} setOpenPanel={setOpenPanel} />}
             <div className="flex items-center h-full justify-between px-4 py-3 bg-white overflow-auto">
                 <div className="flex items-center">
-                    <div className="relative before:content-[''] before:h-[24px] before:border-l-2 before:absolute before:top-1/2 before:left-14 before:bg-red-500 before:-translate-y-1/2">
-                        <PanelLeft className="ml-4 mr-8 h-[36px]" />
-                    </div>
+                    {typeMessage === 'room' && (
+                        <div className="relative before:content-[''] before:h-[24px] before:border-l-2 before:absolute before:top-1/2 before:left-14 before:bg-red-500 before:-translate-y-1/2">
+                            <PanelLeft
+                                onClick={() => setOpenPanel(true)}
+                                className="ml-4 mr-8 h-[36px] cursor-pointer"
+                            />
+                        </div>
+                    )}
                     <img
                         src="https://tse3.mm.bing.net/th/id/OIP.cGz8NopJvAgdkioxkugKoQHaHa?pid=Api&P=0&h=220"
                         alt="hình ảnh"
