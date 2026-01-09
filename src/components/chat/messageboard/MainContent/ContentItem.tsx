cc484943f1e7a8c1e0470ddee4c1909f9d99d2a8
PhamThanhTai-22130242 <22130242@st.hcmuaf.edu.vn>
2026-01-09 14:33:42 +0700

    'fix'

--- src/components/chat/messageboard/MainContent/ContentItem.tsx
+++ src/components/chat/messageboard/MainContent/ContentItem.tsx
@@ -1,8 +1,7 @@
 import { ChatMessage } from '../../../../model/ChatMessage';
 import FileItem from './FileItem';
 import FileImage from './FileImage';
-                      import { 
-                        , CameraOff, Phone, PhoneOff } from 'lucide-react';
+
 
 import { formatAnyTimeToVN } from '../../../../helps/formatServerTimeVN';
 interface ItemContent {
