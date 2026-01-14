e7e185790007d10da59e1e130d6fd8a86a6e5bfa
Le Tuan Phat <21129858@st.hcmuaf.edu.vn>
2026-01-14 19:02:13 +0700

    hoàn thành chức năng notification message

--- src/pages/ChatAppPage.tsx
+++ src/pages/ChatAppPage.tsx
@@ -21,6 +21,7 @@ import RejectModal from '../components/modal/RejectModal';
 import CallModal from '../components/modal/CallModal';
 import { createContext } from 'react';
 import { ListConversationProvider } from '../components/chat/Context/ListConversation';
+import { initMessageNotification } from '../helps/notification';
 
 export interface ICallContext {
     setModalCalling: React.Dispatch<React.SetStateAction<boolean>>;
@@ -47,6 +48,13 @@ function Home() {
     // }, [user.username, navigate]);
     const dispatch = useDispatch();
     const selection = useSelector((state: RootState) => state.call);
+
+    useEffect(() => {
+        async function init() {
+            const check = await initMessageNotification();
+        }
+        init();
+    }, []);
 
     return (
         <>
